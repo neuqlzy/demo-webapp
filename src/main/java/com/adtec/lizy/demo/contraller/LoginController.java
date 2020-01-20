9cad881ce1fe1eb16a46d6188575a60dc5e44da7
lizeyang <lizy@adtec.com.cn>
2020-01-20 16:46:14 +0800

    添加打印日志工具logback

--- src/main/java/com/adtec/lizy/demo/contraller/LoginController.java
+++ src/main/java/com/adtec/lizy/demo/contraller/LoginController.java
@@ -1,14 +1,17 @@
 package com.adtec.lizy.demo.contraller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+    private Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @RequestMapping("/login.do")
+    @RequestMapping("/login")
     public String login() {
-        System.out.println("LoginController.login()");
+        log.info("LoginController.login(), ==> login.jsp 登录");
         return "login";
     }
 }
