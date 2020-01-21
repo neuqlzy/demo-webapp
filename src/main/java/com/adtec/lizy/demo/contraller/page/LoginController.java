85e9467630a6e65b493d3d257de164168cd36bfb
lizeyang <lizy@adtec.com.cn>
2020-01-21 10:42:13 +0800

    调整controller结构

--- src/main/java/com/adtec/lizy/demo/contraller/page/LoginController.java
+++ src/main/java/com/adtec/lizy/demo/contraller/page/LoginController.java
@@ -1,4 +1,4 @@
-package com.adtec.lizy.demo.contraller;
+package com.adtec.lizy.demo.contraller.page;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/page")
 public class LoginController {
     private Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping("/login")
     public String login() {
-        log.info("LoginController.login(), ==> login.jsp ??");
+        log.info("LoginController.login(), ==> login.jsp 登录");
         return "login";
     }
 }
--- src/main/java/com/adtec/lizy/demo/contraller/servlet/json/HelloJsonController.java
+++ src/main/java/com/adtec/lizy/demo/contraller/servlet/json/HelloJsonController.java
@@ -1,17 +1,16 @@
-package com.adtec.lizy.demo.restcontroller;
+package com.adtec.lizy.demo.contraller.servlet.json;
 
 import com.adtec.lizy.demo.model.HelloJsonRequest;
 import com.adtec.lizy.demo.model.HelloJsonResponse;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/servlet/json",
+        produces = "application/json;charset=UTF-8",
+        consumes = "application/json;charset=UTF-8" )
 public class HelloJsonController {
 
-    @RequestMapping(value = "/helloJson", method = RequestMethod.POST,
-            produces = "application/json;charset=UTF-8")
+    @PostMapping("/helloJson")
     public HelloJsonResponse helloJson(@RequestBody HelloJsonRequest request) {
         HelloJsonResponse response = new HelloJsonResponse();
         response.setAuthor("李泽阳");
